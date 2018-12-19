6b8b8ef9c8985049a97c5226a90015016dcfe106
Long Le <longlt00502@gmail.com>
2018-12-19 19:33:39 +0700

    reformat

--- libs/LMngr.hpp
+++ libs/LMngr.hpp
@@ -18,89 +18,78 @@ class Export
 {
 public:
 
-	Export()
-	{
-	}
+    Export()
+    {
+    }
 
-	Export(Export const &aOther)
-	{
-	}
+    Export(Export const &aOther)
+    {
+    }
 
-	Export(Export &&aOther)
-	{
-	}
+    Export(Export &&aOther)
+    {
+    }
 
-	virtual int onHandle(std::string const &)=0;
-	virtual int onInit(void *)=0;
-	virtual void onDeinit(void *)=0;
+    virtual int onHandle(std::string const &)=0;
+    virtual int onInit(void *)=0;
+    virtual void onDeinit(void *)=0;
 };
 
 class LMngr
 {
 public:
 
-	LMngr() {}
+    LMngr() {}
 
-	virtual ~LMngr() 
-	{
-		for(auto lp : _exportContainer)
-		{
-			delete lp;
-		}
-	}
+    virtual ~LMngr() 
+    {
+        for(auto lp : _exportContainer)
+        {
+            delete lp;
+        }
+    }
 
-	virtual void init(void *aPtr=nullptr)
-	{
-		// _onFormat.connect_member(&_format, &Format::onHandle);
-		// _onExport.connect_member(&_export, &Export::onHandle);
-		_onInit.emit(aPtr);
-	}
+    virtual void init(void *aPtr=nullptr)
+    {
+        _onInit.emit(aPtr);
+    }
 
+    virtual void deInit(void *aPtr=nullptr)
+    {
+        _onDeinit.emit(aPtr);
+    }
 
-	virtual void deInit(void *aPtr=nullptr)
-	{
-		// _onFormat.connect_member(&_format, &Format::onHandle);
-		// _onExport.connect_member(&_export, &Export::onHandle);
-		_onDeinit.emit(aPtr);
-	}
+    void add(Export *aExport)
+    {
+        _exportContainer.push_back(aExport);
+        Export *lIns = _exportContainer.back();
+        size_t lId = _onExport.connect(Simple::slot(lIns, &Export::onHandle));
+        lId = _onInit.connect(Simple::slot(lIns, &Export::onInit));
+        lId = _onDeinit.connect(Simple::slot(lIns, &Export::onDeinit));
+    }
 
-	virtual void disconnect()
-	{
-		// _onFormat.disconnect();
-		// _onExport.disconnect();
-	}
-
-	void add(Export *aExport)
-	{
-		_exportContainer.push_back(aExport);
-		Export *lIns = _exportContainer.back();
-		size_t lId = _onExport.connect(Simple::slot(lIns, &Export::onHandle));
-		lId = _onInit.connect(Simple::slot(lIns, &Export::onInit));
-		lId = _onDeinit.connect(Simple::slot(lIns, &Export::onDeinit));
-	}
-
-	void log(int aLvl, const char *fmt, ...);
+    void log(int aLvl, const char *fmt, ...);
 
 private:
-	// Simple::Signal<std::string (char const *, ...)> _onFormat;
-	Simple::Signal<int (std::string const &)> _onExport;
-	Simple::Signal<int (void *)> _onInit;
-	Simple::Signal<void (void *)> _onDeinit;
+    // Simple::Signal<std::string (char const *, ...)> _onFormat;
+    Simple::Signal<int (std::string const &)> _onExport;
+    Simple::Signal<int (void *)> _onInit;
+    Simple::Signal<void (void *)> _onDeinit;
 
-	std::vector<Export *> _exportContainer;
+    std::vector<Export *> _exportContainer;
 };
 
 void LMngr::log(int aLvl, const char *fmt, ...)
 {
-	std::string lBuff;
-	char str[MAX_BUF_SIZE];
-	va_list args;
-	va_start (args, fmt);
+    std::string lBuff;
+    char str[MAX_BUF_SIZE];
+    va_list args;
+    va_start (args, fmt);
 
-	if (vsnprintf (str, sizeof str, fmt, args) >= 0) lBuff = str;
-	va_end (args);
+    if (vsnprintf (str, sizeof str, fmt, args) >= 0) lBuff = str;
+    va_end (args);
 
-	_onExport.emit(lBuff);
+    _onExport.emit(lBuff);
 }
 
 #endif // LMNGR_HPP_
--- tests/test_lmngr.cpp
+++ tests/test_lmngr.cpp
@@ -7,57 +7,57 @@
 class EConsole : public Export
 {
 public:
-	int onInit(void *aPtr) { return 0; }
-	void onDeinit(void *aPtr) { }
-	int onHandle(std::string const &aBuff) { std::cout<<aBuff; }
+    int onInit(void *aPtr) { return 0; }
+    void onDeinit(void *aPtr) { }
+    int onHandle(std::string const &aBuff) { std::cout<<aBuff; }
 };
 
 class EFile : public Export
 {
 public:
-	EFile(std::string const& aFile)
-	{
-		_f = aFile;
-	}
+    EFile(std::string const& aFile)
+    {
+        _f = aFile;
+    }
 
-	int onInit(void *aPtr)
-	{
-		ofs.open(_f, std::ios::out | std::ios::app );
-		if(!ofs.is_open()) return 1;
-		return 0; 
-	}
+    int onInit(void *aPtr)
+    {
+        ofs.open(_f, std::ios::out | std::ios::app );
+        if(!ofs.is_open()) return 1;
+        return 0; 
+    }
 
-	void onDeinit(void *aPtr)
-	{
-		if(ofs.is_open())
-		{
-			ofs.flush();
-			ofs.close();
-		}
-	}
+    void onDeinit(void *aPtr)
+    {
+        if(ofs.is_open())
+        {
+            ofs.flush();
+            ofs.close();
+        }
+    }
 
-	int onHandle(std::string const &aBuff) 
-	{ 
-		ofs << aBuff;
-	}
+    int onHandle(std::string const &aBuff) 
+    { 
+        ofs << aBuff;
+    }
 
 private:
-	std::ofstream ofs;
-	std::string _f;
+    std::ofstream ofs;
+    std::string _f;
 };
 
 
 int main()
 {
-	LMngr log;
-	log.add(new EConsole());
-	log.add(new EFile("log.txt"));
+    LMngr log;
+    log.add(new EConsole());
+    log.add(new EFile("log.txt"));
 
-	log.init(nullptr);
+    log.init(nullptr);
 
-	log.log(0, "%s %s %d\n", __FILE__, __FUNCTION__, __LINE__);
+    log.log(0, "%s %s %d\n", __FILE__, __FUNCTION__, __LINE__);
 
-	log.deInit();
-	LOGD("");
-	return 0;
+    log.deInit();
+    LOGD("");
+    return 0;
 }
