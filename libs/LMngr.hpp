df9c3d39134fcbe857dadb07fe5a0b46579b0c7c
Long Le <longlt00502@gmail.com>
2018-12-20 10:34:35 +0700

    add __format

--- libs/LMngr.hpp
+++ libs/LMngr.hpp
@@ -5,8 +5,24 @@
 #include <libs/SimpleSignal.hpp>
 #include <libs/log.hpp>
 
+#include <iostream>
+#include <fstream>
+
 #include <vector>
 #include <cstdarg>
+
+#include <cstdint>
+#include <cstdlib>
+#include <cstdio>
+#include <cstring>
+
+#include <sys/types.h> 
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <fcntl.h>
+#include <unistd.h> // close
+#include <netdb.h>
+
 
 class LMngr;
 
@@ -30,9 +46,108 @@ public:
     {
     }
 
-    virtual int onHandle(std::string const &)=0;
+    virtual int onHandle(int aLvl, std::string const &)=0;
     virtual int onInit(void *)=0;
     virtual void onDeinit(void *)=0;
+};
+
+class EConsole : public Export
+{
+public:
+    int onInit(void *aPtr) { return 0; }
+    void onDeinit(void *aPtr) { }
+    int onHandle(int aLvl, std::string const &aBuff) { std::cout<<aBuff; return 0; }
+};
+
+class EFile : public Export
+{
+public:
+    EFile(std::string const &aFile) : _f (aFile) {}
+    EFile(std::string &&aFile) : _f (std::move(aFile)) {}
+
+    int onInit(void *aPtr)
+    {
+        ofs.open(_f, std::ios::out | std::ios::app );
+        if(!ofs.is_open()) return 1;
+        return 0;
+    }
+
+    void onDeinit(void *aPtr)
+    {
+        if(ofs.is_open())
+        {
+            ofs.flush();
+            ofs.close();
+        }
+    }
+
+    int onHandle(int aLvl, std::string const &aBuff)
+    { 
+        ofs << aBuff;
+    }
+
+private:
+    std::ofstream ofs;
+    std::string _f;
+};
+
+class ENetUDP : public Export
+{
+public:
+    ENetUDP(std::string &&aHost, uint16_t aPort) : _host(std::move(aHost)), _port(aPort) {}
+
+    ENetUDP(std::string const &aHost, uint16_t aPort) : _host(aHost), _port(aPort) {}
+
+    int onInit(void *aPtr)
+    {
+        _fd = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
+        
+        if ( _fd <= 0 )
+        {
+            LOGD("");
+            return 1;
+        }
+
+        struct hostent *lpSvr;
+        lpSvr = gethostbyname(_host.data());
+
+        _svrAddr.sin_family = AF_INET;
+        _svrAddr.sin_port = htons( _port );
+        bcopy((char *)lpSvr->h_addr, (char *)&_svrAddr.sin_addr.s_addr, lpSvr->h_length);
+
+        // set non-blocking io
+        if ( fcntl( _fd, F_SETFL, O_NONBLOCK, 1 ) == -1 )
+        {
+            LOGD( "failed to make socket non-blocking" );
+            return 1;
+        }
+
+        return 0;
+    }
+
+    void onDeinit(void *aPtr)
+    {
+        if(_fd > 0) close(_fd);
+    }
+
+    int onHandle(int aLvl, std::string const &aBuff)
+    {
+        size_t sent_bytes = sendto( _fd, aBuff.data(), aBuff.size(), 0, (sockaddr*)&_svrAddr, sizeof(sockaddr_in) );
+
+        if(!sent_bytes) 
+        {
+            LOGD("");
+            return 1;
+        }
+
+        return 0;
+    }
+
+private:
+    std::string _host;
+    uint16_t _port;
+    int _fd;
+    sockaddr_in _svrAddr;
 };
 
 class LMngr
@@ -70,8 +185,16 @@ public:
 
     void log(int aLvl, const char *fmt, ...);
 
+    std::string __format(char const *aFormat, va_list &aVars) const
+    {
+        std::string lBuff;
+        char str[MAX_BUF_SIZE];
+        if (vsnprintf (str, sizeof str, aFormat, aVars) >= 0) lBuff = str;
+        return std::move(lBuff);
+    }
+
 private:
-    Simple::Signal<int (std::string const &)> _onExport;
+    Simple::Signal<int (int, std::string const &)> _onExport;
     Simple::Signal<int (void *)> _onInit;
     Simple::Signal<void (void *)> _onDeinit;
 
@@ -80,15 +203,12 @@ private:
 
 void LMngr::log(int aLvl, const char *fmt, ...)
 {
-    std::string lBuff;
-    char str[MAX_BUF_SIZE];
     va_list args;
     va_start (args, fmt);
-
-    if (vsnprintf (str, sizeof str, fmt, args) >= 0) lBuff = str;
+    std::string lBuff = __format(fmt, args);
     va_end (args);
 
-    _onExport.emit(lBuff);
+    _onExport.emit(aLvl, lBuff);
 }
 
 #endif // LMNGR_HPP_
