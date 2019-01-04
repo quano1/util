3dcabf1fd0355ac7069135bc458d0cc23bb717cb
Long Le <longlt00502@gmail.com>
2019-01-05 01:26:16 +0700

    separate to C header

---
+++ libs/log.h
@@ -0,0 +1,22 @@
+#pragma once
+
+struct LogMngr;
+struct Exporter;
+struct Tracer;
+
+void log_async(struct LogMngr *aLogger, int aLogType, const char *fmt, ...);
+void log_init(struct LogMngr *aLogger);
+void log_deinit(struct LogMngr *aLogger);
+
+void start_trace(struct LogMngr *aLogger, struct Tracer **aTracer, char const *aBuf);
+void stop_trace(struct Tracer *aTracer);
+
+#define LOGD(format, ...) printf("[Dbg] %s %s %d " format "\n", __FILE__, __func__, __LINE__, ##__VA_ARGS__)
+
+#define TRACES(logger, FUNC) struct Tracer *__##FUNC; log_async(logger, 3, #FUNC " %s %d", __FUNCTION__, __LINE__); start_trace(logger, &__##FUNC, #FUNC)
+#define TRACEE(FUNC) stop_trace(__##FUNC)
+
+#define LOGI(logger, fmt, ...) log_async(logger, 0, "%s %s %d " fmt "", __FILE__, __FUNCTION__, __LINE__, ##__VA_ARGS__)
+#define LOGW(logger, fmt, ...) log_async(logger, 1, "%s %s %d " fmt "", __FILE__, __FUNCTION__, __LINE__, ##__VA_ARGS__)
+#define LOGE(logger, fmt, ...) log_async(logger, 2, "%s %s %d " fmt "", __FILE__, __FUNCTION__, __LINE__, ##__VA_ARGS__)
+
--- tests/test_lmngr.c
+++ tests/test_lmngr.c
@@ -1,43 +1,23 @@
 
 #include <stdio.h>
-
-struct LogMngr;
-struct Exporter;
-struct Tracer;
-
-void log_async(struct LogMngr *aLogger, int aLogType, const char *fmt, ...);
-void log_init(struct LogMngr *aLogger);
-void log_deinit(struct LogMngr *aLogger);
-
-void start_trace(struct LogMngr *aLogger, struct Tracer **aTracer, char const *aBuf);
-void stop_trace(struct Tracer *aTracer);
-
-#define LOGD(format, ...) printf("[Dbg] %s %s %d " format "\n", __FILE__, __func__, __LINE__, ##__VA_ARGS__)
-
-#define TRACE_S(logger, FUNC) struct Tracer *__##FUNC; log_async(logger, 3, #FUNC " %s %d", __FUNCTION__, __LINE__); start_trace(logger, &__##FUNC, #FUNC)
-#define TRACE_E(FUNC) stop_trace(__##FUNC)
-
-#define LOGI(logger, fmt, ...) log_async(logger, 0, "%s %s %d " fmt "", __FILE__, __FUNCTION__, __LINE__, ##__VA_ARGS__)
-#define LOGW(logger, fmt, ...) log_async(logger, 1, "%s %s %d " fmt "", __FILE__, __FUNCTION__, __LINE__, ##__VA_ARGS__)
-#define LOGE(logger, fmt, ...) log_async(logger, 2, "%s %s %d " fmt "", __FILE__, __FUNCTION__, __LINE__, ##__VA_ARGS__)
+#include <log.h>
 
 struct LogMngr *lp;
-
 
 int main()
 {
 	// struct Tracer *tr;
 	log_init(&lp);
 	// start_trace(lp, &tr, "MAIN");
-	TRACE_S(lp, MAIN);
+	TRACES(lp, MAIN);
 
 	LOGI(lp, "");
-	TRACE_S(lp, MAIN2);
+	TRACES(lp, MAIN2);
 	LOGW(lp, "");
-	TRACE_E(MAIN2);
+	TRACEE(MAIN2);
 	LOGE(lp, "");
 
-	TRACE_E(MAIN);
+	TRACEE(MAIN);
 
 	log_deinit(lp);
 	return 0;
