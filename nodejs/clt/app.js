94663039f0b7af0a19670c1bee6904bc7f7c869b
Long Le <longlt00502@gmail.com>
2019-01-06 16:40:44 +0700

    add cltudp

---
+++ nodejs/clt/app.js
@@ -0,0 +1,12 @@
+
+angular.module('lltlog', ['ngMaterial'])
+.controller('AppCtrl', function($scope) {
+
+});
+
+
+/**
+Copyright 2018 Google LLC. All Rights Reserved. 
+Use of this source code is governed by an MIT-style license that can be found
+in the LICENSE file at http://material.angularjs.org/HEAD/license.
+**/
