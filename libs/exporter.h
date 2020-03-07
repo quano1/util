4f4d8d9a5fd6c525756e78f768e5f8394094a32b
Long Le <longlt00502@gmail.com>
2020-03-08 03:51:33 +0700

    add omp

--- libs/exporter.h
+++ libs/exporter.h
@@ -19,6 +19,8 @@
 #include <functional>
 #include "utils.h"
 
+#include <omp.h>
+
 #ifndef STATIC_LIB
 #define UTIL_INLINE
 #else
@@ -37,7 +39,7 @@ union LogFlag {
         uint32_t reserve : 28;
         uint32_t debug : 1;
     };
-    uint32_t value;
+    uint32_t flag;
 };
 
 enum class LogType : uint32_t
@@ -49,6 +51,7 @@ enum class LogType : uint32_t
 };
 
 typedef std::pair<LogType, std::string> LogInfo;
+typedef std::pair<int, LogFlag> LogFd;
 
 template <size_t const kLogSize, uint32_t max_log_queue, uint32_t const kDelay>
 class Logger
@@ -63,6 +66,10 @@ public:
     {
         is_running_ = false;
         if(broadcast_thread.joinable()) broadcast_thread.join();
+        for(auto lfd : lfds_)
+        {
+            close(lfd.first);
+        }
     }
 
     UTIL_INLINE void start()
@@ -80,16 +87,21 @@ public:
                 }
 
                 LogInfo log_message;
-                lf_queue_.pop([](LogInfo &elem, uint32_t, LogInfo &log_msg)
+                lf_queue_.pop([&log_message](LogInfo &elem, uint32_t)
                 {
-                    log_msg = std::move(elem);
-                }, log_message);
+                    log_message = std::move(elem);
+                });
 
-                for(auto fd : fds_)
+                // for(auto fd : fds_)
+                // omp_set_num_threads(fds_.size());
+                // #pragma omp parallel
+                #pragma omp parallel for
+                for(int i=0; i<lfds_.size(); i++)
                 {
-                    if((uint32_t)fd.second.value & (uint32_t)log_message.first)
+                    LogFd &lfd = lfds_[i];
+                    if((uint32_t)lfd.second.flag & (uint32_t)log_message.first)
                     {
-                        auto size = write(fd.first, log_message.second.data(), log_message.second.size());
+                        auto size = write(lfd.first, log_message.second.data(), log_message.second.size());
                     }
                 }
             }
@@ -109,26 +121,35 @@ public:
         {
             elem = std::move(log_msg);
         }, LogInfo{type, utils::Format(kLogSize, format, std::forward<Args>(args)...)});
+
+        if(!is_running_) start();
     }
 
-    template <typename ... Fds>
-    void addFd(std::pair<int, LogFlag> const &fd, Fds ...fds)
+    template <typename ... LFds>
+    void addFd(LFds ...lfds)
     {
         if(is_running_) return;
-        fds_[fd.first] = fd.second;
-        addFd(fds...);
+        _addFd(lfds...);
     }
 
-    UTIL_INLINE void addFd(std::pair<int, LogFlag> const &fd)
+private:
+    template <typename ... LFds>
+    void _addFd(LogFd lfd, LFds ...lfds)
     {
-        if(is_running_) return;
-        fds_[fd.first] = fd.second;
+        lfds_.push_back(lfd);
+        _addFd(lfds...);
+    }
+
+    UTIL_INLINE void _addFd(LogFd lfd)
+    {
+        lfds_.push_back(lfd);
     }
 
     utils::BSDLFQ<LogInfo> lf_queue_;
     bool is_running_;
     std::thread broadcast_thread;
-    std::unordered_map<int, LogFlag> fds_;
+    // std::unordered_map<int, LogFlag> fds_;
+    std::vector<LogFd> lfds_;
 };
 
 } // llt
--- tests/crtptest.cc
+++ tests/crtptest.cc
@@ -7,7 +7,7 @@
 #include "../libs/exporter.h"
 // #include "../libs/exporterudp.h"
 
-#define LOG_HEADER utils::Format("%ld:%s:%s:%d[%s]", utils::timestamp(), __FILE__, __FUNCTION__, __LINE__, utils::tid())
+#define LOG_HEADER utils::Format("%ld:%s:%s:%d[%s]", utils::timestamp<std::chrono::high_resolution_clock, std::micro>(), __FILE__, __FUNCTION__, __LINE__, utils::tid())
 
 #define LLT_LOGT(logger, format, ...) (logger).log<LogType::kTrace>("[T]%s(" format ")\n", LOG_HEADER , ##__VA_ARGS__)
 
@@ -25,37 +25,26 @@ namespace {
 int main(int argc, char const *argv[])
 {
 	using namespace llt;
-	int fdraw1 = open("crtptest1.log", O_WRONLY | O_TRUNC /*| O_CREAT*/ , 0644);
-	int fdraw2 = open("crtptest2.log", O_WRONLY | O_TRUNC /*| O_CREAT*/ , 0644);
-	int fdraw3 = open("crtptest3.log", O_WRONLY | O_TRUNC /*| O_CREAT*/ , 0644);
-
-	// LConsole console;
-	// console.onInit();
-	// console.onLog(LogInfo{});
-	// std::string str = utils::Format("%d %s\n",10, "oi troi oi");
-	// write(0, str.data(), str.size());
-
-	// LOGD("%ld", sizeof(LogFlag));
-	// LogFlag lt; lt.value = 0;
-	// lt.fatal = 1;
-	// LOGD("%x", lt.value);
-	// LOGD("%x", lt.trace);
-	// LOGD("%x", lt.info);
-	// LOGD("%x", lt.fatal);
-	// LOGD("%x", lt.debug);
-	// LOGD("%x", lt.reserve);
-
-	// return 0;
+	int fdraw1 = open("crtptest.t.log", O_WRONLY /*| O_TRUNC*/ | O_CREAT , 0644);
+	LOGD("%d", fdraw1);
+	int fdraw2 = open("crtptest.i.log", O_WRONLY /*| O_TRUNC*/ | O_CREAT , 0644);
+	LOGD("%d", fdraw2);
+	int fdraw3 = open("crtptest.f.log", O_WRONLY /*| O_TRUNC*/ | O_CREAT , 0644);
+	LOGD("%d", fdraw3);
 
 	if(argc == 2)
 	{
 		TIMER(logger);
 		Logger<0x400, 0x1000, 0> lg;
-		lg.addFd({1, LogFlag{.debug=(1U)}});
-		lg.addFd({fdraw1, LogFlag{.trace=(1U)}});
-		lg.addFd({fdraw2, LogFlag{.info=(1U)}});
-		lg.addFd({fdraw3, LogFlag{.fatal=(1U)}});
-		lg.start();
+		// lg.addFd({1, LogFlag{.debug=(1U)}});
+		lg.addFd(LogFd{fdraw1, LogFlag{.trace=(1U)}}, 
+		         LogFd{fdraw2, LogFlag{.info=(1U)}}, 
+		         LogFd{fdraw3, LogFlag{.fatal=(1U)}});
+
+		// lg.addFd({fdraw2, LogFlag{.info=(1U)}});
+		// lg.addFd({fdraw3, LogFlag{.fatal=(1U)}});
+		// lg.start();
+
 		for(int i=0; i<std::stoi(argv[1]); i++)
 		{
 			LLT_LOGT(lg, "%d %s", 10, "oi troi oi");
