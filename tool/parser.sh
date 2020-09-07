a431f688af288396923e812ef1afebf337e1d829
Long Le <longlt00502@gmail.com>
2020-09-07 15:49:20 +0700

    update parser

--- tool/parser.sh
+++ tool/parser.sh
@@ -1,112 +1,160 @@
 #!/bin/bash
 
 # head -n 9 test_lmngr.log | \
-
-eof_=0
-
 # while [[ eof_ -eq 0 ]]; do
 # sort -t "}" -k2 | \
-[[ -z $2 ]] && color_enable=0 || color_enable=1
 
-dd if=$1 iflag=skip_bytes,count_bytes,nonblock bs=4K skip=0 count=50M 2> /tmp/null | sort -t "}" -k 2,2 -s |\
-sed -e 's#^.\(.*\).$#\1#g' | \
-awk -F  "}{" '
-BEGIN {
-    color_enable='"$color_enable"'
-    if (color_enable == 1) {
-        RESET="\033[0m"
-        split("\033[91m;\033[92m;\033[93m;\033[94m;\033[95m;\033[96m;\033[97m;\033[31m;\033[32m;\033[33m;\033[34m;\033[35m;\033[36m;\033[37m;", colorlist, ";")
-    }
-    tcolor = 1;
-}
+[[ -z $4 ]] && color_enable=0 || color_enable=1
+
+function parse()
 {
-    type_=$1
-    time_=$2
-    thread_=$3
-    lvl_=$4-1
-    prev_ctx=$5
-    curr_ctx=$6
-    # if (lvl_ > 4) next;
-    # split($5, obj_, ":")
+    # total_lines=`dd if=${1} iflag=skip_bytes,count_bytes,nonblock bs=${2} skip=${3} count=${4} | wc -l $1`;
+    # sed -e 's#^.\(.*\).$#\1#g' | \
 
-    if (color_enable == 1) {
-        if (thread_color_[thread_] == "") {
-            thread_color_[thread_] = colorlist[tcolor];
-            tcolor+=1;
-            if(tcolor > length(colorlist)) {
-                tcolor = 1;
+    dd if=${1} iflag=skip_bytes,count_bytes,nonblock bs=${2} skip=${3} count=${4} 2> /tmp/null | \
+    sort -t "}" -k 2,2 -s | \
+    gawk -F  "}{" '
+    BEGIN {
+        # print "BEGIN"
+        color_enable='"$color_enable"'
+        if (color_enable == 1) {
+            RESET="\033[0m"
+            split("\033[91m;\033[92m;\033[93m;\033[94m;\033[95m;\033[96m;\033[97m;\033[31m;\033[32m;\033[33m;\033[34m;\033[35m;\033[36m;\033[37m;", colorlist, ";")
+        }
+        tcolor = 1;
+        total_size = 0;
+    }
+    {
+        type_=$1
+        time_=$2
+        thread_=$3
+        lvl_=$4
+        # split($5, ctxs, ":");
+        prev_ctx=$5
+        curr_ctx=$6
+        # if (lvl_ > 4) next;
+        # split($5, obj_, ":")
+
+        if (color_enable == 1) {
+            if (thread_color_[thread_] == "") {
+                thread_color_[thread_] = colorlist[tcolor];
+                tcolor+=1;
+                if(tcolor > length(colorlist)) {
+                    tcolor = 1;
+                }
+            }
+
+            switch (type_) {
+                case "{T":
+                    type_color_ = "\033[1;44m";
+                    type_text_= RESET;
+                    lvl_-=1;
+                    break;
+                case "{I":
+                    type_color_ = "\033[1;42m";
+                    type_text_= RESET;
+                    # lvl_++;
+                    break;
+                case "{W":
+                    type_color_ = "\033[1;43m"
+                    type_text_= "\033[1m";
+                    # lvl_++;
+                    break;
+                case "{F":
+                    type_color_ = "\033[1;41m"
+                    type_text_= "\033[7m";
+                    # lvl_++;
+                    break;
+                default:
+                    type_color_ = RESET;
+                    type_text_= RESET;
+                    # lvl_++;
+                    break;
             }
         }
 
-        switch (type_) {
-            case "T":
-                type_color_ = "\033[1;44m";
-                type_text_= RESET;
-                break;
-            case "I":
-                type_color_ = "\033[1;42m";
-                type_text_= RESET;
-                lvl_++;
-                break;
-            case "W":
-                type_color_ = "\033[1;43m"
-                type_text_= "\033[1m";
-                lvl_++;
-                break;
-            case "F":
-                type_color_ = "\033[1;41m"
-                type_text_= "\033[7m";
-                lvl_++;
-                break;
-            default:
-                type_color_ = RESET;
-                type_text_= RESET;
-                lvl_++;
-                break;
+        # print match($0, "}$")
+
+        if(NF < 8 || match($0, "}$") == 0)
+        {
+            print "ERROR: "$0;
+            # print total_size > ".run.log"
+            # exit 1;
+        } else {
+
+            if(NF == 9) {
+                func_=$7;
+                line_=$8;
+                msg_=$9;
+
+                printf "{%s}%s%s}%s%s{%s}%2d%.*s{%s}{%s %s}%s %s{%s%s\n", 
+                    time_, type_color_, type_, RESET, thread_color_[thread_], thread_, lvl_, (lvl_), "-----------------------------", 
+                    curr_ctx, func_, line_, RESET, 
+                    type_text_, msg_, RESET;
+            }
+            else if (NF == 8) {
+                if (color_enable == 1) {
+                    type_color_ = "\033[46m";
+                }
+                func_=$7
+                msg_=$8;
+                printf "{%s}%s%s}%s%s{%s}%2d%.*s{%s} %s%s {%s\n", 
+                        time_, type_color_, type_, RESET, thread_color_[thread_], thread_, lvl_, (lvl_), "-----------------------------", 
+                        curr_ctx, RESET, func_, msg_;
+                # print $0
+                # print NF
+            }
+            # total_size += length+1;
         }
     }
-
-    if(NF > 8) {
-        func_=$7;
-        line_=$8;
-        msg_=$9;
-
-        if (type_ == "T") {
-            printf "{%s}%s{%s}%s%s{%s}%2d%.*s{%s}{%s %s}%s %s\n", 
-                time_, type_color_, type_, RESET, thread_color_[thread_], thread_, lvl_, (lvl_), "-----------------------------", 
-                curr_ctx, func_, line_, RESET, msg_;
-
-            # stack_lvl_[thread_]=lvl_;
-        }
-        else {
-            printf "{%s}%s{%s}%s%s{%s}%2d%.*s{%s}{%s %s}%s%s{%s}%s\n", 
-                time_, type_color_, type_, RESET, thread_color_[thread_], thread_, lvl_, (lvl_), "-----------------------------", 
-                curr_ctx, func_, line_, RESET, 
-                type_text_, msg_, RESET;
-        }
-    }
-    else if (NF == 8) {
-        if (color_enable == 1) {
-            type_color_ = "\033[46m";
-        }
-        #printf "{%s}%s{%s}%s%s{%s}%2d%*s{%s}%s%s{%s}{%s}%s\n", time_, type_color_, type_, RESET, thread_color_[thread_], thread_, lvl_, lvl_, " ", curr_ctx, RESET, type_text_, $6, $7, RESET;
-        func_=$7
-        msg_=$8;
-        printf "{%s}%s{%s}%s%s{%s}%2d%.*s{%s}{%s %s}%s\n", 
-                time_, type_color_, type_, RESET, thread_color_[thread_], thread_, lvl_, (lvl_), "-----------------------------", 
-                curr_ctx, func_, msg_, RESET;
-    }
-    else {
-      printf "ERROR: %s\n",$0
-    }
-
+    END {
+        # for (key in stack_lvl_) {
+        #     printf "%d\n", stack_lvl_[key]/2 > "/tmp/.t_"key;
+        #     close("/tmp/.t_"key)
+        # }
+        # print total_size + NR * 3;
+        # print total_size;
+        # print "END"
+    }';
+    # echo $total_lines;
 }
-END {
-    # for (key in stack_lvl_) {
-    #     printf "%d\n", stack_lvl_[key]/2 > "/tmp/.t_"key;
-    #     close("/tmp/.t_"key)
-    # }
-}
-'
-eof_=1
+
+# total_size=`ls -l $1 | awk '{print $5}'`;
+block_=1024;
+# skip_=0;
+# cnt_=1024;
+
+# cnt_=1048576;
+parse $3 $block_ $1 $2;
+# exit 1;
+
+# total_size=`expr $total_size - $block_`;
+# echo $total_size;
+# rm -f run.log;
+
+# while [[ $skip_ -le $total_size ]]; do
+# for i in {1..5}; do
+#     # echo $skip_ " " $cnt_;
+#     tmp_=$cnt_;
+#     parse $1 $block_ $skip_ $cnt_ >> run.log;
+#     # echo $?
+#     # echo $?;
+#     # ls -l run.log | awk '{print $5}';
+#     # if [[ ! -z .run.log ]]; then
+#     if [[ $? -eq 1 ]]; then
+#         tmp_=`cat .run.log`;
+#         rm -f .run.log;
+#     fi
+#     skip_=`expr $skip_ + $cnt_ - $cnt_ + $tmp_`;
+#     echo $skip_;
+#     # skip_=`ls -l run.log | awk '{print $5}'`;
+#     # cnt_=`expr $skip_ + $block_`;
 # done
+
+# # echo $skip_ " " $cnt_;
+# tmp_=$skip_;
+# if [[ ! -z .run.log ]]; then
+#     tmp_=`cat .run.log`;
+#     rm -f .run.log;
+# fi
+# parse $1 $block_ $skip_ $cnt_ >> run.log
