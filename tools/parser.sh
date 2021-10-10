e06ed5f33aa44ada554396733818cb860e94bc74
thanh long le <longlt00502@gmail.com>
2021-10-10 18:57:26 +0700

    Update parser
    new format parsed message

--- tools/parser.sh
+++ tools/parser.sh
@@ -28,55 +28,56 @@ function parse()
     {
         type_=$1
         time_=$2+DIFF_
-        int_time_=int(time_);
-        time_=int((time_ - int_time_) * int(1e9));
         thread_=int($3)
         lvl_=$4
-        curr_ctx=$5
+        curr_ctx_=$5
+        int_time_=int(time_);
+        time_=int((time_ - int_time_) * int(1e9));
         date_=strftime("%y-%b-%d %T",int_time_);
 
-        if (COLOR_ENABLED_ == 1) {
-
-            if (thread_color_[thread_] == "") {
-                index_ = (thread_%length(colorlist)) + 1;
-                thread_color_[thread_] = colorlist[index_];
-            }
-
-            switch (type_) {
-                case "{T":
-                    type_color_ = "\033[1;44m";
-                    type_text_= RESET;
-                    break;
-                case "{I":
-                    type_color_ = "\033[1;42m";
-                    type_text_= RESET;
-                    break;
-                case "{W":
-                    type_color_ = "\033[1;43m"
-                    type_text_= "\033[1m";
-                    break;
-                case "{F":
-                    type_color_ = "\033[1;41m"
-                    type_text_= "\033[7m";
-                    break;
-                default:
-                    type_color_ = RESET;
-                    type_text_= RESET;
-                    break;
-            }
-        }
-
         if(NF == 8) {
-            func_=$6;
-            line_=$7;
-            msg_=$8;
 
-            if(type_ == "{T") {
+            if (COLOR_ENABLED_ == 1) {
+
+                if (thread_color_[thread_] == "") {
+                    index_ = (thread_%length(colorlist)) + 1;
+                    thread_color_[thread_] = colorlist[index_];
+                }
+
+                switch (type_) {
+                    case "T":
+                        type_color_ = "\033[1;44m";
+                        type_text_= RESET;
+                        break;
+                    case "I":
+                        type_color_ = "\033[1;42m";
+                        type_text_= RESET;
+                        break;
+                    case "W":
+                        type_color_ = "\033[1;43m"
+                        type_text_= "\033[1m";
+                        break;
+                    case "F":
+                        type_color_ = "\033[1;41m"
+                        type_text_= "\033[7m";
+                        break;
+                    default:
+                        type_color_ = RESET;
+                        type_text_= RESET;
+                        break;
+                }
+            }
+
+            func_line_=sprintf("%s %s", $6, $7);;
+            # line_=$7;
+            msg_=$8;
+            trace_diff_=""
+            if(type_ == "T") {
 
                 if(match(msg_, "^+")) {
                     traces_[thread_][lvl_]=$2
                 } else {
-                    trace_diff_=sprintf("%.9f (s)", $2 - traces_[thread_][lvl_]);
+                    trace_diff_=sprintf("%9d (ns)", ($2 - traces_[thread_][lvl_])*1e9);
                     if(COLOR_ENABLED_ == 1) {
                         type_color_ = "\033[46m";
                     }
@@ -84,18 +85,29 @@ function parse()
 
             } else {
 
-                if(lvl_ > 0) {
-                    trace_diff_=sprintf("%.9f (s)", $2 - traces_[thread_][lvl_-1]);
-                } else {
-                    trace_diff_=""
-                }
+                # if(lvl_ > 0) {
+                #     trace_diff_=sprintf("%9d (ns)", ($2 - traces_[thread_][lvl_-1])*1e9);
+                # } else {
+                #     trace_diff_=""
+                # }
 
             }
 
-            printf "{%s.%d}%s%s}%s%s{%s}%2d%.*s{%s}{%s %s}%s %s{%s%s %s\n",
-                date_, time_, type_color_, type_, RESET, thread_color_[thread_], thread_, lvl_, (lvl_), "-----------------------------",
-                curr_ctx, func_, line_, RESET,
-                type_text_, msg_, RESET, trace_diff_;
+            date_time_=sprintf("%s.%d", date_, time_);
+            shift_=sprintf("%.*s", (lvl_), "                               ");
+            # msg_=sprintf("%s%s%s", type_text_, msg_, RESET);
+            printf "%s" \
+                "%s {%20.20s} %20.20s %-20.20s%s " \
+                "%s{%s}%s" \
+                "%s"\
+                "%s{%s}%s "\
+                "%s\n",
+                date_time_, 
+                thread_color_[thread_], thread_, curr_ctx_, func_line_, RESET,
+                type_color_, type_, RESET,
+                shift_,
+                thread_color_[thread_], msg_, RESET,
+                trace_diff_;
 
         } else {
             print "ERROR: "$0;
@@ -115,7 +127,7 @@ main() {
 DIFF_=`head -n1 $1 | awk '{print $2 - $1;}'`
 # echo $DIFF_;
 
-tail -n +2 $1 | sort -t "}" -k 2,2 -s | parse $DIFF_
+tail -n +2 $1 | sort -t "}" -k 2,2 -s | sed -e 's#{\(.*\)}#\1#' | parse $DIFF_
 }
 
 main $*
